624b8a031557a84743da62e358c7b546a6c3040f
Shakleen Ishfar <shakleenishfar@gmail.com>
2022-01-10 13:19:27 +0600

    Completed player walking state

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -8,26 +8,31 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Show debug log texts")]
     [SerializeField] private bool _showDebugText = false;
 
-    [Tooltip("Maximum veloctiy the player can walk")]
-    [SerializeField] [Range(0.5f, 5.0f)] private float _maxVelocityWalk = 1.0f;
+    // -------------------------------------------------------------------------------------------------------------------------------------------------
+    [Header("Walk Variables")]
+    [Tooltip("Maximum forward veloctiy the player can walk")] [SerializeField] [Range(0.5f, 5.0f)] private float _maxForwardWalkVelocity = 1.0f;
+    public float MaxForwardWalkVelocity { get { return _maxForwardWalkVelocity; } }
+    [Tooltip("Maximum backward veloctiy the player can walk")] [SerializeField] [Range(-5f, -0.5f)] private float _maxBackwardWalkVelocity = -1.0f;
+    public float MaxBackwardWalkVelocity { get { return _maxBackwardWalkVelocity; } }
+    [Tooltip("Accelaration used to go towards maximum walk velocity")] [SerializeField] [Range(0.5f, 5.0f)] private float _accelarationWalk = 2.0f;
+    public float AccelarationWalk { get { return _accelarationWalk; } }
+    [Tooltip("Deceleration used to go towards idle or zero velocity")] [SerializeField] [Range(0.5f, 5.0f)] private float _decelarationWalk = 2.0f;
+    public float DecelarationWalk { get { return _decelarationWalk; } }
+    // -------------------------------------------------------------------------------------------------------------------------------------------------
 
-    [Tooltip("Accelaration used to go towards maximum walk velocity")]
-    [SerializeField] [Range(0.5f, 5.0f)] private float _accelarationWalk = 1.0f;
-
-    [Tooltip("Deceleration used to go towards idle or zero velocity")]
-    [SerializeField] [Range(0.5f, 5.0f)] private float _decelarationWalk = 1.0f;
-
-    [Tooltip("Maximum veloctiy the player can run")]
-    [SerializeField] [Range(0.5f, 5.0f)] private float _maxVelocityrun = 2.0f;
-
-    [Tooltip("Accelaration used to go towards maximum run velocity")]
-    [SerializeField] [Range(0.5f, 5.0f)] private float _accelarationRun = 2.0f;
-
-    [Tooltip("Deceleration used to go towards max walk velocity")]
-    [SerializeField] [Range(0.5f, 5.0f)] private float _decelarationRun = 1.0f;
+    // -------------------------------------------------------------------------------------------------------------------------------------------------
+    [Header("Run Variables")]
+    [Tooltip("Maximum veloctiy the player can run")] [SerializeField] [Range(0.5f, 5.0f)] private float _maxVelocityRun = 2.0f;
+    public float MaxVelocityRun { get { return _maxVelocityRun; } }
+    [Tooltip("Accelaration used to go towards maximum run velocity")] [SerializeField] [Range(0.5f, 5.0f)] private float _accelarationRun = 2.0f;
+    public float AccelarationRun { get { return _accelarationRun; } }
+    [Tooltip("Deceleration used to go towards max walk velocity")] [SerializeField] [Range(0.5f, 5.0f)] private float _decelarationRun = 1.0f;
+    public float DecelarationRun { get { return _decelarationRun; } }
+    // -------------------------------------------------------------------------------------------------------------------------------------------------
 
     // Component references
     private Animator _animator;
+    public Animator Animator { get { return _animator; } }
     private CharacterController _controller;
     private InputActions _inputActions;
     private PlayerStateManager _stateManager;
@@ -38,11 +43,40 @@ public class PlayerController : MonoBehaviour
     private PlayerBaseState _currentState;
     public PlayerBaseState CurrentState { get { return _currentState; } set { _currentState = value; } }
 
+    // Animator hashes
+    private int _animatorHashVelocityX;
+    public int AnimatorHashVelocityX { get { return _animatorHashVelocityX; } }
+    private int _animatorHashVelocityZ;
+    public int AnimatorHashVelocityZ { get { return _animatorHashVelocityZ; } }
+
     // Movement variables
+    [SerializeField] private Vector3 _currentMovementVelocity = Vector3.zero;
+    public Vector3 CurrentMovementVelocity { get { return _currentMovementVelocity; } set { _currentMovementVelocity = value; } }
+    public float CurrentMovementVelocityX 
+    { 
+        get { return _currentMovementVelocity.x; } 
+        set 
+        { 
+            _currentMovementVelocity.x = value; 
+            _animator.SetFloat("velocityX", value);
+        } 
+    }
+    public float CurrentMovementVelocityZ 
+    { 
+        get { return _currentMovementVelocity.z; } 
+        set 
+        { 
+            _currentMovementVelocity.z = value; 
+            _animator.SetFloat("velocityZ", value);
+        } 
+    }
+
     [SerializeField] private bool _isInputActiveMovement = false;
     public bool IsInputActiveMovement { get { return _isInputActiveMovement; } }
+
     [SerializeField] private bool _isInputActiveRun = false;
     public bool IsInputActiveRun { get { return _isInputActiveRun; } }
+    
     [SerializeField] private Vector2 _inputMovementVector = Vector2.zero;
     public Vector2 InputMovementVector { get { return _inputMovementVector; } }
 
@@ -80,10 +114,18 @@ public class PlayerController : MonoBehaviour
         _isInputActiveRun = context.ReadValueAsButton();
     }
 
+    private void Start() 
+    {
+        // Setting animation hashes
+        _animatorHashVelocityX = Animator.StringToHash("velocityX");
+        _animatorHashVelocityX = Animator.StringToHash("velocityZ");
+    }
+
     private void Update() 
     {
         _currentState.ExecuteState();
         if (!_currentState.hasPrint) Debug.Log($"Player in state: {_currentState.GetName()}");
+        _controller.Move(_currentMovementVelocity * Time.deltaTime);
     }
 
     private void OnDisable() { _inputActions.Disable(); }
--- Assets/Player/Scripts/PlayerWalkState.cs
+++ Assets/Player/Scripts/PlayerWalkState.cs
@@ -1,5 +1,9 @@
+using UnityEngine;
+
 public class PlayerWalkState : PlayerBaseState
 {
+    private const float THRESH = 1e-5f;
+
     public PlayerWalkState(PlayerController context, PlayerStateManager manager) : base(context, manager) {}
 
     public override void OnEnterState() { hasPrint = false; }
@@ -8,13 +12,64 @@ public class PlayerWalkState : PlayerBaseState
 
     public override void CheckSwitchState() 
     {
-        if (!_context.IsInputActiveMovement)
+        if (!_context.IsInputActiveMovement && _context.CurrentMovementVelocity == Vector3.zero)
             SwitchState(_manager.GetIdleState());
         else if (_context.IsInputActiveRun)
             SwitchState(_manager.GetRunState());
     }
 
-    public override void ExecuteState() { CheckSwitchState(); }
+    public override void ExecuteState() 
+    { 
+        CheckSwitchState(); 
+        _context.CurrentMovementVelocityX = ChangeAxisVelocity(_context.InputMovementVector.x, _context.CurrentMovementVelocityX);
+        _context.CurrentMovementVelocityZ = ChangeAxisVelocity(_context.InputMovementVector.y, _context.CurrentMovementVelocityZ);
+    }
+
+    private float ChangeAxisVelocity(float inputVelocity, float currentVelocity)
+    {
+        float velocity = currentVelocity;
+
+        if (inputVelocity > 0)
+        {
+            // Player pressed button to move in the positive direction of axis
+            velocity = currentVelocity + ApplyFrameIndependentAccelaration(inputVelocity);
+            velocity = Mathf.Clamp(velocity, 0, MaxAllowablePositiveVelocity(inputVelocity));
+        }
+        else if (inputVelocity < 0)
+        {
+            // Player pressed button to move in the negative direction of axis
+            velocity = currentVelocity - ApplyFrameIndependentAccelaration(inputVelocity);
+            velocity = Mathf.Clamp(velocity, MaxAllowableNegativeVelocity(inputVelocity), 0);
+        }
+        else
+        {
+            if (currentVelocity > 0)
+            {
+                // Player hasn't pressed any button for this axis but was moving in the positive direction.
+                velocity = currentVelocity - ApplyFrameIndependentDecelaration();
+                velocity = Mathf.Clamp(velocity, 0, _context.MaxForwardWalkVelocity);
+            }
+            else if (currentVelocity < 0)
+            {
+                // Player hasn't pressed any button for this axis but was moving in the negative direction.
+                velocity = currentVelocity + ApplyFrameIndependentDecelaration();
+                velocity = Mathf.Clamp(velocity, _context.MaxBackwardWalkVelocity, 0);
+            }
+        }
+        
+
+        return velocity;
+    }
+
+    private float MaxAllowableNegativeVelocity(float inputVelocity) { return _context.MaxBackwardWalkVelocity * Mathf.Abs(inputVelocity); }
+
+    private float MaxAllowablePositiveVelocity(float inputVelocity) { return _context.MaxForwardWalkVelocity * Mathf.Abs(inputVelocity); }
+
+    private float ApplyFrameIndependentAccelaration(float inputVelocity) { return _context.AccelarationWalk * Mathf.Abs(inputVelocity) * Time.deltaTime; }
+
+    private float ApplyFrameIndependentDecelaration() { return _context.DecelarationWalk * Time.deltaTime; }
+
+    private static bool IsVelocityZero(float inputVelocity) { return (inputVelocity > -THRESH) || (inputVelocity < THRESH); }
 
     public override string GetName()
     {
