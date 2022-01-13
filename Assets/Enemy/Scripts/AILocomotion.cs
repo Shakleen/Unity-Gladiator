f282820fb364aa07eeebdd5dc62197c605984db6
Shakleen Ishfar <shakleenishfar@gmail.com>
2022-01-13 15:33:32 +0600

    Added enemy movement animation and debugging

--- Assets/Enemy/Scripts/AILocomotion.cs
+++ Assets/Enemy/Scripts/AILocomotion.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,12 +7,35 @@ public class AILocomotion : MonoBehaviour
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private Animator _animator;
+    
+    [Tooltip("Amount of time to pass before the next path update is called.")]
+    [SerializeField] [Range(0.2f, 10.0f)] private float _minimumUpdateWaitTime = 1.0f;
+    
+    [Tooltip("Minimum amount of units player should travel in order to call a path update.")]
+    [SerializeField] [Range(1.0f, 10.0f)] private float _minimumUpdateDistance = 5.0f;
 
-    private void Update() { FollowPlayer(); }
+    private int _animationHashMovementSpeed;
 
-    private void FollowPlayer()
+    private void Awake()
     {
-        _navMeshAgent.destination = _playerTransform.position;
-        _animator.SetFloat("Speed", _navMeshAgent.velocity.magnitude);
+        _animationHashMovementSpeed = Animator.StringToHash("MovementSpeed");
+        StartCoroutine(UpdatePlayerFollowPath());
     }
+
+    private IEnumerator UpdatePlayerFollowPath()
+    {
+        float thresh = _minimumUpdateDistance * _minimumUpdateDistance;
+        
+        while(true)
+        {
+            float playerMoveDistance = (_playerTransform.position - _navMeshAgent.destination).sqrMagnitude;
+            
+            if (playerMoveDistance > thresh)
+                _navMeshAgent.destination = _playerTransform.position;
+
+            yield return new WaitForSeconds(_minimumUpdateWaitTime);
+        }
+    }
+
+    private void Update() { _animator.SetFloat(_animationHashMovementSpeed, _navMeshAgent.velocity.magnitude); }
 }
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ A 3rd person action game where the player goes through hordes of enemies in a gl
 2. [Axe Model from 3D RPG pack, Unity Store](https://assetstore.unity.com/packages/3d/props/weapons/free-rpg-weapons-199738)
 3. [iHeartGameDev](https://www.youtube.com/c/iHeartGameDev)
 4. [samyam](https://www.youtube.com/c/samyam)
+5. [TheKiwiCoder](https://www.youtube.com/channel/UCjszZMwnOW4fO5VIDU_Wh1Q)
